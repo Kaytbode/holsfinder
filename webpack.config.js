c0000db003a86b6890beeaedb37ac68084624db9
kaytbode <bkaoluborode@gmail.com>
2019-09-16 16:25:53 +0100

    refactor: Remove html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app/index.jsx',
@@ -22,19 +21,15 @@ module.exports = {
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: path.join(__dirname, 'src/'),
     port: 3000,
     publicPath: 'http://localhost:3000/dist/',
     hotOnly: true
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    })
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
