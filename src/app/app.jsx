879a8d5525e097d00374bf553e1cb29dfdeabcf7
kaytbode <bkaoluborode@gmail.com>
2019-09-16 11:00:16 +0100

    chore: Configure app environment

---
+++ src/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import '../styles/scss/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello, World opgt! </h1>
+    </div>
+  );
+}
+
+export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app/index.jsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$|\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ]
+};
