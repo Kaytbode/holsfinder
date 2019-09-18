36ded74fca92839d0fe016498bd3e316cbf42109
kaytbode <bkaoluborode@gmail.com>
2019-09-18 14:33:41 +0100

    feat: Users can view holidays by country

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -5,14 +5,32 @@ import DisplayHolidays from './displayholidays';
 import '../styles/scss/app.scss';
 import DisplayNavs from './nav';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { country: '', month: '', holidays: { statusCode: 0, data: [] } };
+    this.state = {
+      country: '',
+      month: '',
+      holidays: [],
+      countries: []
+    };
 
-    this.handleCountryChange = this.handleCountryChange.bind(this);
     this.handleMonthChange = this.handleMonthChange.bind(this);
+    this.handleCountryChange = this.handleCountryChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const api = 'https://holsfinderapi.herokuapp.com/api/v1/countries';
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const initialData = data.data.map((value) => value);
+        this.setState({
+          countries: initialData
+        });
+      });
   }
 
   handleCountryChange(event) {
@@ -29,8 +47,9 @@ class App extends Component {
     fetch(api)
       .then((response) => response.json())
       .then((data) => {
+        const initialData = data.data.map((value) => value);
         this.setState({
-          holidays: data
+          holidays: initialData
         });
       });
 
@@ -39,8 +58,12 @@ class App extends Component {
 
   render() {
     const {
-      holidays, country, month
+      holidays, month, country, countries
     } = this.state;
+
+    const optionItems = countries.map((value) => (
+      <option key={value.name} value={value.code}>{value.name}</option>
+    ));
 
     return (
       <div>
@@ -49,21 +72,34 @@ class App extends Component {
           <Form onSubmit={this.handleSubmit}>
             <Row>
               <Col>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Country</Form.Label>
-                  <Form.Control type="text" value={country} onChange={this.handleCountryChange} placeholder="Enter country" />
-                </Form.Group>
+                <Form.Label>Choose Country</Form.Label>
+                <select value={country} onChange={this.handleCountryChange}>
+                  <option value="">Choose Country</option>
+                  { optionItems }
+                </select>
               </Col>
               <Col>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Month</Form.Label>
-                  <Form.Control type="text" value={month} onChange={this.handleMonthChange} placeholder="Enter month" />
-                </Form.Group> 
+                <Form.Label>Choose Month</Form.Label>
+                <select value={month} onChange={this.handleMonthChange}>
+                  <option value="">Choose Month</option>
+                  <option value="1">January</option>
+                  <option value="2">February</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">May</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </select>
               </Col>
             </Row>
             <Row className="justify-content-md-center">
               <Col xs={{ span: 4, offset: 2 }} md={{ span: 4, offset: 0 }}>
-                <Form.Control className="display" type="submit" value="Get Holidays" />
+                <Form.Control className="display" type="submit" value="Find Holidays" />
               </Col>
             </Row>
           </Form>
--- src/app/displayholidays.jsx
+++ src/app/displayholidays.jsx
@@ -1,22 +1,70 @@
 import React, { Component } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
+class DisplayHolidays extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false, holiday: { date: '', name: '' } };
 
-const DisplayHolidays = ({ holidays }) => {
-  return (
-    <ul>
-      {holidays.data.map((country) => (
-        <li key={country.country}>{country.name}</li>
-      ))}
-    </ul>
-  );
-};
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow(event) {
+    const { holidays } = this.props;
+    const { value } = event.target;
+    const holsValue = holidays.filter(({ date }) => (Number(date.split('-')[2])) === Number(value));
+    this.setState({ holiday: holsValue[0], show: true });
+  }
+
+  render() {
+    const { holidays } = this.props;
+    const { show, holiday } = this.state;
+
+    const holidayDates = holidays.map(({ date }) => Number(date.split('-')[2]));
+
+    const daysOfTheMonth = Array.from({ length: 31 }, (v, i) => i + 1);
+
+    return (
+      <div className="container">
+        {
+            daysOfTheMonth.map((val) => {
+              if (holidayDates.indexOf(val) > -1) {
+                return (
+                  <div key={val} className="green-div">
+                    <Button onClick={this.handleShow} value={val}>{val}</Button>
+                  </div>
+                );
+              }
+              return <div key={val} className="white-div">{val}</div>;
+            })
+          }
+        <Modal show={show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Holiday Description</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+             <p> Date: { holiday.date }</p>
+             <p> Name: { holiday.name }</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+                Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 DisplayHolidays.propTypes = {
-  holidays: PropTypes.shape({
-    statusCode: PropTypes.number,
-    data: PropTypes.array
-  }).isRequired
+  holidays: PropTypes.arrayOf.isRequired
 };
 
 export default DisplayHolidays;
